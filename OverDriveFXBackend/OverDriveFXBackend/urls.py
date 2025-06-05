53aa4bebd07c8035e447a70229a84ca4c54e07fa
Xbox-hash <duotech.com.py@gmail.com>
2025-06-05 00:40:02 -0300

    correccion template login

--- OverDriveFXBackend/OverDriveFXBackend/urls.py
+++ OverDriveFXBackend/OverDriveFXBackend/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('login/', LoginView.as_view(template_name = 'login.html'), name='login')
-
 ]
